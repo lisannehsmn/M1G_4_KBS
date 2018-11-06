b20e813e7f2373deac3e50c594596916cde994a6
YouriZomer98 <44570521+YouriZomer98@users.noreply.github.com>
2018-11-06 13:03:36 +0100

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,5 +1,39 @@
 <?php
+function dbGetConfig(){
+   return array(
+    'db' => "mysql:host=localhost;dbname=wideworldimporters;port=3306",
+    'user' => "root",
+    'pass' => ""
+    );
+}
 
-$connect = mysqli_connect("localhost", "root", "", "wideworldimporters")or die("Couldn't connect to server or database");
+function dbSelectAll($sql){
+    $dbconfig= dbGetConfig();
+    $pdo = new PDO($dbconfig['db'], $dbconfig['user'], $dbconfig['pass']);
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute();
+    $pdo = null;
+    return $stmt;
+}
 
+function dbSelect($sql, $waardes){
+    $dbconfig= dbGetConfig();
+    $pdo=new PDO($dbconfig['db'], $dbconfig['user'], $dbconfig['pass']);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute($waardes);
+    $pdo = null;
+    return $stmt;
+}
+function getStock(){
+    $sql = "SELECT * FROM stockitems";
+    return dbSelectAll($sql);
+}
+function getGroup(){
+    $sql = "SELECT * FROM stockgroups";
+    return dbSelectAll($sql);
+}
+function getSearch(){
+    $sql = "SELECT * FROM stockitems where StockItemName LIKE '%". $_GET['search'] ."%' ";
+    return dbSelectAll($sql);
+}
 ?>
