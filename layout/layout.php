b881ef68a35a589dcab5f83368c107cfff9b258a
BlackEagle75x <33255033+BlackEagle75x@users.noreply.github.com>
2018-11-06 15:14:27 +0100

    Add files via upload

---
+++ layout/layout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+<nav class="navbar navbar-toggleable-md bg-dark">
+  <a class="navbar-brand" href="#"><img src="wwi.png" width="160" height="48" class="d-inline-block align-top" alt=""></a>
+  <button type="button" class="button-style btn btn-warning btn-sm text-white">Login</button>
+</nav>
+    
+    
+    
+    
+    
+<footer class="footer small text-center">
+    <div class="container">
+        Copyright © Wide World Importers 2018
+    </div>
+</footer>
+    
+</body>
+</html>
