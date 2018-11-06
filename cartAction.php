db5327e1c2dd3739a69e6d5021e8e65db98abc0d
Shahin419 <shahen_thebeste@hotmail.com>
2018-11-06 18:18:27 +0100

    Add files via upload

---
+++ cartAction.php
@@ -0,0 +1,54 @@
+<?php
+// initialiseer winkelwagenklasse
+include 'Cart.php';
+$cart = new Cart;
+
+// include database
+include 'connect.php';
+if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
+    if($_REQUEST['action'] == 'addToCart' && !empty($_REQUEST['id'])){
+        $productID = $_REQUEST['id'];
+        // ontvang productdetails
+        $query = $connect->query("SELECT * FROM stockitems WHERE id = ".$productID);
+        $row = $query->fetch_assoc();
+        $itemData = array(
+            'id' => $row['id'],
+            'name' => $row['name'],
+            'price' => $row['price'],
+            'qty' => 1
+        );
+        
+        $insertItem = $cart->insert($itemData);
+        $redirectLoc = $insertItem?'moreproduct.php':'moreproduct.php';
+        header("Location: ".$redirectLoc);
+    }elseif($_REQUEST['action'] == 'updateCartItem' && !empty($_REQUEST['id'])){
+        $itemData = array(
+            'rowid' => $_REQUEST['id'],
+            'qty' => $_REQUEST['qty']
+        );
+        $updateItem = $cart->update($itemData);
+        echo $updateItem?'ok':'err';die;
+    }elseif($_REQUEST['action'] == 'removeCartItem' && !empty($_REQUEST['id'])){
+        $deleteItem = $cart->remove($_REQUEST['id']);
+        header("Location: viewCart.php");
+    }elseif($_REQUEST['action'] == 'placeOrder' && $cart->total_items() > 0 && !empty($_SESSION['sessCustomerID'])){
+
+        if($insertOrder){
+            $orderID = $db->insert_id;
+            $sql = '';
+            // krijg winkelwagenitems
+            $cartItems = $cart->contents();
+            foreach($cartItems as $item){
+                $sql .= "INSERT INTO order_items (order_id, product_id, quantity) VALUES ('".$orderID."', '".$item['id']."', '".$item['qty']."');";
+            }
+            
+            if($insertOrderItems){
+                $cart->destroy();
+            }else{
+            }
+        }else{
+        }
+    }else{
+    }
+}else{
+}
