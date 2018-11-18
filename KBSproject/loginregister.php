2235ad0994b1c2e7b047fdae7f45803b07fff8ac
IcyEaglz <44570150+IcyEaglz@users.noreply.github.com>
2018-11-18 13:47:03 +0100

    Add files via upload

---
+++ KBSproject/loginregister.php
@@ -0,0 +1,73 @@
+<?php
+//include the login script
+include("logconnection.php");
+include("regconnection.php");
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Login wideworldimporters</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head> 
+    <body>
+        <div class="header">
+            <h2>Log hier in</h2>
+        <form method ="post" name="login">
+    <div class="input-group">
+        <div class="input-group-prepend">
+        <span class="input-group-text" id="email">Email</span>
+        </div>
+        <input type="text" class="form-control" aria-label="email" aria-describedby="email" name="email" p>
+            </div>
+            <br>
+            
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+        <span class="input-group-text" id="password">Password</span>
+            </div>
+        <input class="form-control" type="password" aria-label="password" aria-describedby="password "name="userPassword">
+            </div>
+            <button class="btn btn-success" type="submit" name="login_btn">Login</button>
+            </form>
+                            <!--Hier ga je de registratie naast login zetten-->
+    <br><br><br><br> 
+            <form method="post" name="register">
+                <?php include('errors.php');?>
+            <h2>Schrijf je in!</h2>
+    <div class="input-group">
+        <label for="name">Volledige naam*</label>
+        <input type="text" name="name" class="textInput" placeholder="Naam en achternaam">
+    </div>
+    
+    <div class="input-group">
+        <label for="email">Email*</label> 
+        <input type="email" name="email" class="textInput" placeholder="Email">
+    </div>
+    
+    <div class="input-group">
+    <label for="address">Adres*</label>
+        <input type="text" name="address" class="textInput" placeholder="Adres">
+    </div>
+    
+    <div class="input-group">
+        <label for="number">Telefoonnummer</label>
+        <input type="text" name="number" class="textInput" placeholder="Nummer">
+    </div>
+            
+    <div class="input-group">
+        <label for="password">Wachtwoord*</label>
+        <input type="password" name="password" class="textInput" placeholder="Wachtwoord">
+    </div>
+    
+    <div class="input-group">
+        <label for="password2">Wachtwoord*</label>
+        <input type="password" name="password2" class="textInput" placeholder="Herhaal wachtwoord">
+    </div>
+    <p>Alle velden zijn verplicht met *</p>    
+            <div class="input-group">
+                <button type="submit" class="btn btn-success" name="register_btn" id="register_btn">Registreer</button>
+    </div>
+        </form>
+        </div>
+    </body>
+</html>
---
+++ KBSproject/regconnection.php
@@ -0,0 +1,56 @@
+<?php
+// Variable errors maken
+$errors=array();
+    
+if (isset($_POST['register_btn'])){
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+    $address = filter_input(INPUT_POST, "address", FILTER_SANITIZE_EMAIL);
+    $number = filter_input(INPUT_POST, "number", FILTER_SANITIZE_STRING);
+    $password = $_POST["password"];
+    $password2 = $_POST["password2"];
+
+
+    //Errors
+    if (empty($name)) {
+        array_push($errors, "Naam is verplicht");
+    }
+    if (empty($email)) {
+        array_push($errors, "Email is verplicht");
+    }
+    if (empty($password) || empty($password2)) {
+        array_push($errors, "Wachtwoord is verplicht");
+    }
+    if ($password != $password2) {
+        array_push($errors, "De wachtwoorden komen niet overeen");
+    }
+    if (strlen($password) > 10 || strlen($password2 > 10 )){
+        array_push($errors, "Je wachtwoord is langer dan 10 karakters");
+    }
+    if (empty($address)){
+        array_push($errors, "Adres is verplicht");
+    }
+    
+    //Registreer gebruiker bij 0 errors:
+    if (count($errors)==0){
+        $password = md5($password); //Hash <--
+        $password2 = md5($password2);
+
+    
+    try{
+        $con = new PDO ("mysql:host=localhost;dbname=wideworldimporters", "root", "");
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $con->prepare("INSERT INTO users (name, email, address, number, password, password2) VALUES('$name','$email','$address','$number','$password', '$password2')");
+        $stmt->execute();
+        
+    } catch(PDOException $e){
+    echo "error".$e->getMessage();
+        print_r($errors);
+    }
+    $con = null;
+    header("location: loginregister.php");
+
+}
+}
+?>
+
