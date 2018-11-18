29976734fc3b882050d576fd612e19b6ff6902ba
BlackEagle75x <33255033+BlackEagle75x@users.noreply.github.com>
2018-11-18 01:04:27 +0100

    Update loginregister.php

--- loginregister.php
+++ loginregister.php
@@ -2,72 +2,116 @@
 //include the login script
 include("logconnection.php");
 include("regconnection.php");
+include 'KBSproject/header.php';
+
 ?>
-<!DOCTYPE HTML>
-<html>
     <head>
         <title>Login wideworldimporters</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    </head> 
-    <body>
-        <div class="header">
-            <h2>Log hier in</h2>
-        <form method ="post" name="login">
-    <div class="input-group">
-        <div class="input-group-prepend">
-        <span class="input-group-text" id="email">Email</span>
-        </div>
-        <input type="text" class="form-control" aria-label="email" aria-describedby="email" name="email" p>
-            </div>
-            <br>
-            
+        <style>
+        .box1
+        {
+            padding-right: 2em;
+            max-width: 49% !important;
+
+        }
+        .box2
+        {
+            padding-left: 2em;
+        }
+
+        .line{
+            border: 1px solid #c3c1bc;
+        }
+            .error
+            {
+                font-family: sans-serif;
+                color: #E70000;
+                font-size: 16px;
+                line-height: 2px;
+            }
+        </style>
+
+
+<div class="container">
+        <div class="row">
+        <div class="col-md-6 box1">
+            <h4>Inloggen</h4>
+            <p>Bestaande klanten</p>
+            <form method ="post" name="login">
             <div class="input-group mb-3">
-                <div class="input-group-prepend">
-        <span class="input-group-text" id="password">Password</span>
+            <div class="input-group-prepend"><span class="input-group-text" id="email">Email</span></div>
+            <input type="text" class="form-control" aria-label="email" aria-describedby="email" name="email" placeholder="E-mailadres">
             </div>
-        <input class="form-control" type="password" aria-label="password" aria-describedby="password "name="userPassword">
+
+            <div class="input-group mb-3">
+             <div class="input-group-prepend">
+                <span class="input-group-text" id="password">Wachtwoord</span></div>
+                <input class="form-control" type="password" aria-label="password" aria-describedby="password" name="userPassword" placeholder="Wachtwoord">
             </div>
-            <button class="btn btn-success" type="submit" name="login_btn">Login</button>
+            <button class="btn btn-success" type="submit" name="login_btn">Inloggen</button>
             </form>
-                            <!--Hier ga je de registratie naast login zetten-->
-    <br><br><br><br> 
-            <form method="post" name="register">
-                <?php include('errors.php');?>
-            <h2>Schrijf je in!</h2>
-    <div class="input-group">
-        <label for="name">Volledige naam*</label>
-        <input type="text" name="name" class="textInput" placeholder="Naam en achternaam">
-    </div>
-    
-    <div class="input-group">
-        <label for="email">Email*</label> 
-        <input type="email" name="email" class="textInput" placeholder="Email">
-    </div>
-    
-    <div class="input-group">
-    <label for="address">Adres*</label>
-        <input type="text" name="address" class="textInput" placeholder="Adres">
-    </div>
-    
-    <div class="input-group">
-        <label for="number">Telefoonnummer</label>
-        <input type="text" name="number" class="textInput" placeholder="Nummer">
-    </div>
-            
-    <div class="input-group">
-        <label for="password">Wachtwoord*</label>
-        <input type="password" name="password" class="textInput" placeholder="Wachtwoord">
-    </div>
-    
-    <div class="input-group">
-        <label for="password2">Wachtwoord*</label>
-        <input type="password" name="password2" class="textInput" placeholder="Herhaal wachtwoord">
-    </div>
-    <p>Alle velden zijn verplicht met *</p>    
-            <div class="input-group">
-                <button type="submit" class="btn btn-success" name="register_btn" id="register_btn">Registreer</button>
-    </div>
-        </form>
         </div>
-    </body>
-</html>
+
+        <div class="line"></div>
+
+            <!--Hier ga je de registratie naast login zetten-->
+        <div class="col-md-6 box2">
+            <form method="post" name="register">
+            <h4>Nieuw bij wwi.nl?</h4>
+            <p>Maak binnen 2 minuten een account aan.</p>
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                    <span class="input-group-text" for="name">Volledige naam*</span>
+                    </div>
+                    <input type="text" name="name" class="form-control textInput" placeholder="Naam en achternaam">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" for="email">Email*</span>
+                    </div>
+                    <input type="email" name="email" class="form-control textInput" placeholder="Email">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" for="address">Adres*</span>
+                    </div>
+                    <input type="text" name="address" class="form-control textInput" placeholder="Adres">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" for="number">Telefoonnummer</span>
+                    </div>
+                    <input type="text" name="number" class="form-control textInput" placeholder="Nummer">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" for="password">Wachtwoord*</span>
+                    </div>
+                    <input type="password" name="password" class="form-control textInput" placeholder="Wachtwoord">
+                </div>
+
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" for="password2">Wachtwoord*</span>
+                    </div>
+                        <input type="password" name="password2" class="form-control textInput" placeholder="Herhaal wachtwoord">
+                </div>
+                <p>Alle velden zijn verplicht met *</p>
+                <?php include('errors.php');?>
+                 <div class="input-group">
+                    <button type="submit" class="btn btn-success" name="register_btn" id="register_btn">Maak een account aan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+ </div>
+<?php include 'KBSproject/footer.php';?>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
