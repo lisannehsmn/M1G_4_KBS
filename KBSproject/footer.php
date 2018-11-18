1132e39a92ce0fee26c23aef3aa7811db194fbb1
BlackEagle75x <33255033+BlackEagle75x@users.noreply.github.com>
2018-11-18 01:05:23 +0100

    Update footer.php

--- KBSproject/footer.php
+++ KBSproject/footer.php
@@ -1,4 +1,16 @@
-         <footer class="footer small text-center">
+<style>
+    .footer {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        height: 60px; /* Set the fixed height of the footer here */
+        line-height: 60px; /* Vertically center the text there */
+        background-color: #343a40;
+        font-size: 17px;
+        color:white;
+    }
+</style>         
+<footer class="footer small text-center">
             <div class="footer">
                 Copyright © Wide World Importers 2018
             </div>
